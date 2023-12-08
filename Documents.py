4cca497624265a280cdca5864b6c9b06edf481fc
vedsu <90701239+vedsu@users.noreply.github.com>
2023-12-08 02:46:06 -0500

    Add files via upload

--- Documents.py
+++ Documents.py
@@ -4,21 +4,16 @@ import pymongo
 # import time
 from datetime import datetime, timedelta
 # import pytz
-# from io import BytesIO
+from io import BytesIO
 import base64
+
+
 
 
 @st.cache_resource
 def init_connection():
     try:
-        db_username = st.secrets.db_username
-        db_password = st.secrets.db_password
-
-        mongo_uri_template = "mongodb+srv://{username}:{password}@cluster0.thbmwqi.mongodb.net/"
-        mongo_uri = mongo_uri_template.format(username=db_username, password=db_password)
-
-        client = pymongo.MongoClient(mongo_uri)
-        
+        client = pymongo.MongoClient("mongodb+srv://Vedsu:CVxB6F2N700cQ0qu@cluster0.thbmwqi.mongodb.net/")
         return client
     except Exception as e:
         st.error(f"Error raised, {str(e)}")
@@ -28,33 +23,72 @@ db = client['EventDatabase']
 collection = db['Webinars']
 
 def main():
-    st.subheader("Document Viewer")
-    try:
-        # Perform the update
-        events = collection.find({}).sort("Date", pymongo.ASCENDING)
-
-        # st.write(events)
-    except Exception as e:
-        st.error(f"Error {str(e)}")
-    col1, col2=st.columns(2)
-    count=0
-    for event in events:
-        webinar = event.get("Webinar")
-        status = event.get('Status')
-        document = event.get("Document")
-        if document !=None:
-            # Display PDF content as a link
-            pdf_content_b64 = base64.b64encode(document).decode('utf-8')
-            pdf_href = f'<a href="data:application/pdf;base64,{pdf_content_b64}" target="_blank">View PDF</a>'
-        else:
-            pdf_href = None
-
-
-        if webinar!="Holiday":
-            count+=1
-            with col1:
-                # Extract the part before the first occurrence of '-' or ':'
-                
-                st.caption(f"{count} . {webinar} : {pdf_href}", unsafe_allow_html=True)
+    # Create columns with specified widths
+    col1, col2 = st.columns([3, 7])  
             
-                # st.caption(pdf_href, unsafe_allow_html=True)
+    with col1:
+            st.subheader("Upload document to Webinar")
+
+    
+            # Query MongoDB to retrieve webinar data
+            webinars = collection.find({"Webinar": {"$ne": "Holiday"}, "Status": {"$ne": "Completed"}})
+            # Extract webinar titles from the query result
+            webinar_titles = ["Select Webinar Title"] + [webinar["Webinar"] for webinar in webinars]
+            # Streamlit app
+            selected_webinar_title = st.selectbox("Select Webinar Title", webinar_titles)
+
+            # Display selected webinar title
+            st.write(f"Selected Webinar Title: {selected_webinar_title}")
+            # File upload
+            
+            uploaded_file = st.file_uploader("Choose a PDF file")
+
+            if uploaded_file is not None:
+                st.success("File uploaded successfully!")
+
+                # Read PDF content
+                pdf_content = BytesIO(uploaded_file.read())
+                document = pdf_content.read()
+
+            
+                try:
+                    result = collection.update_one(
+                                {"Webinar": selected_webinar_title},
+                                    {"$set": {"Document": document}})
+                    if result:
+                        st.info(f"File inserted into MongoDB with Name: {selected_webinar_title}")
+                    else:
+                        st.info(f"File could not be inserted into MongoDB with Name: {selected_webinar_title}")
+
+                except Exception as e:
+                    st.sidebar.error(f'Failed!: {str(e)}')           # st.caption(pdf_href, unsafe_allow_html=True)
+    with col2:
+        st.subheader("Document Viewer")
+        try:
+            # Perform the update
+            events = collection.find({}).sort("Date", pymongo.ASCENDING)
+
+            # st.write(events)
+        except Exception as e:
+            st.error(f"Error {str(e)}")
+        col1, col2=st.columns(2)
+        count=0
+        for event in events:
+            webinar = event.get("Webinar")
+            status = event.get('Status')
+            document = event.get("Document")
+            if status!="Completed":
+                if document !=None:
+                    # Display PDF content as a link
+                    pdf_content_b64 = base64.b64encode(document).decode('utf-8')
+                    pdf_href = f'<a href="data:application/pdf;base64,{pdf_content_b64}" target="_blank">View PDF</a>'
+                else:
+                    pdf_href = None
+
+
+                if webinar!="Holiday":
+                    count+=1
+                    with col1:
+                        # Extract the part before the first occurrence of '-' or ':'
+                        
+                        st.caption(f"{count} . {webinar} : {pdf_href}", unsafe_allow_html=True)
